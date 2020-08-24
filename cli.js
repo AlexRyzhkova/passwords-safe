b3f8ba9734e0e8efc97c0d355ab2bd5c9aa6716f
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-08-24 14:43:14 +0200

    Read password from password.json

--- cli.js
+++ cli.js
@@ -1,12 +1,41 @@
-// console.log("Hallo");
-//import readline from "readline"
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
-readline.question(`What's your password?`, (password) => {
-  console.log(`You password is ${password}!`);
-  hideEchoBack: true;
-  readline.close();
+const questions = [
+  {
+    type: "password",
+    name: "password",
+    message: "What's your master password?",
+  },
+  {
+    type: "input",
+    name: "key",
+    message: "Which password do you need?",
+  },
+  //   {
+  //     type: "list",
+  //     name: "task",
+  //     message: "What do you want to do?",
+  //     choices: ["set", "get"],
+  //   },
+];
+
+inquirer.prompt(questions).then(async (answers) => {
+  if (answers.password === "123") {
+    console.log("password correct");
+    try {
+      const passwordsJson = await fs.readFile("./password.json", "utf8");
+      const passwords = JSON.parse(passwordsJson);
+      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
+    } catch (error) {
+      console.error("ERROR");
+    }
+  } else {
+    console.log("wrong password!");
+  }
 });
+// inquirer.prompt(questions).then(async (answers) => {
+//     if (answers.task === "set") {
+// console.log
+
+//     }
