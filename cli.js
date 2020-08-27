3df8f3945fbf51e04e2c45fba6841978bb01aa06
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-08-27 07:19:34 +0200

    "Read and write from database"

--- cli.js
+++ cli.js
@@ -13,42 +13,59 @@ const {
   writeMasterPassword,
 } = require("./lib/passwords");
 const { encrypt, decrypt, createHash, verifyHash } = require("./lib/crypto");
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://AlexaR:EJ8v2tsmANJt6X0R@cluster0.ka893.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 async function main() {
-  const originalMasterPassword = await readMasterPassword();
+  try {
+    await client.connect();
+    const database = client.db("safer_passwords");
 
-  if (!originalMasterPassword) {
-    const { newMasterPassword } = await askForNewMasterPassword();
-    const hashedPassword = createHash(newMasterPassword);
-    await writeMasterPassword(hashedPassword);
-    console.log("Master Password set!");
-    return;
-  }
+    const originalMasterPassword = await readMasterPassword();
 
-  const { masterPassword, action } = await askStartQuestions();
-
-  if (!verifyHash(masterPassword, originalMasterPassword)) {
-    console.log("Master Password is incorrect!");
-    return;
-  }
-
-  console.log("Master Password is correct!");
-  if (action === CHOICE_GET) {
-    console.log("Now Get a password");
-    const { key } = await askGetPasswordQuestions();
-    try {
-      const encryptedPassword = await readPassword(key);
-      const password = decrypt(encryptedPassword, masterPassword);
-      console.log(`Your ${key} password is ${password}`);
-    } catch (error) {
-      console.error("ERROR!");
+    if (!originalMasterPassword) {
+      const { newMasterPassword } = await askForNewMasterPassword();
+      const hashedPassword = createHash(newMasterPassword);
+      await writeMasterPassword(hashedPassword);
+      console.log("Master Password set!");
+      return;
     }
-  } else if (action === CHOICE_SET) {
-    console.log("Now Set a password");
-    const { key, password } = await askSetPasswordQuestions();
-    const encryptedPassword = encrypt(password, masterPassword);
-    await writePassword(key, encryptedPassword);
-    console.log(`New Password set`);
+
+    const { masterPassword, action } = await askStartQuestions();
+
+    if (!verifyHash(masterPassword, originalMasterPassword)) {
+      console.log("Master Password is incorrect!");
+      return;
+    }
+
+    console.log("Master Password is correct!");
+    if (action === CHOICE_GET) {
+      console.log("Now Get a password");
+      const { key } = await askGetPasswordQuestions();
+      try {
+        const encryptedPassword = await readPassword(key, database);
+        const password = decrypt(encryptedPassword, masterPassword);
+        console.log(`Your ${key} password is ${password}`);
+      } catch (error) {
+        console.error("ERROR!");
+      }
+    } else if (action === CHOICE_SET) {
+      console.log("Now Set a password");
+      try {
+        const { key, password } = await askSetPasswordQuestions();
+        const encryptedPassword = encrypt(password, masterPassword);
+        await writePassword(key, encryptedPassword, database);
+        console.log(`New Password set`);
+      } catch (error) {
+        console.error("ERROR");
+      }
+    }
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
 
