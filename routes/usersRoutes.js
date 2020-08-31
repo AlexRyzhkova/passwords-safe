a45b7841e68b1bc590f2f0247fa8be4ba01ceef1
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-08-31 21:58:28 +0200

    "Find user by username"

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 
-function createUsersRouter() {
+function createUsersRouter(database) {
   const router = express.Router();
 
+  const usersCollection = database.collection("users");
+
   router.post("/login", async (request, response) => {
-    const { username } = request.body;
-    console.log(username);
-    response.send("Logged in");
+    try {
+      const { username } = request.body;
+      const user = await usersCollection.findOne({ username });
+      if (!user) {
+        response.status(401).send("Wrong username");
+        return;
+      }
+      console.log(user);
+      response.send("Logged in");
+    } catch (error) {
+      console.error(error);
+      response.status(500).send(error.message);
+    }
   });
 
   return router;
