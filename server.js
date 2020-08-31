281b96af5ce50f9ecd49b58ac42d213b25af9092
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-08-31 10:39:49 +0200

    Add error handling

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ require("dotenv").config();
 
 //distruction classe
 const { MongoClient } = require("mongodb");
-const { readPassword, writePassword } = require("./lib/passwords");
+const { readPassword } = require("./lib/passwords");
 const { decrypt, encrypt } = require("./lib/crypto");
+//Inhalt (body ) wird analysiert un dem wird ein Wert gegeben
 const bodyParser = require("body-parser");
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+});
 const masterPassword = "123";
 const app = express();
+//app.use middelware- alle Anfrgaen werden zuerst in app.use bearbeitet( alle Ruten)
 app.use(bodyParser.json());
 
 async function main() {
@@ -21,10 +25,20 @@ async function main() {
   //die Methode legt den Endpunkt fest
   //:alles was hinter / steht wird unter name gespeichert
   app.get("/api/password/:name", async (request, response) => {
-    const { name } = request.params;
-    const encryptedPassword = await readPassword(name, database);
-    const password = decrypt(encryptedPassword, masterPassword);
-    response.send(password);
+    try {
+      //params url
+      const { name } = request.params;
+      const encryptedPassword = await readPassword(name, database);
+      if (!encryptedPassword) {
+        response.status(404).send(`Password ${name} not found`);
+        return;
+      }
+      const password = decrypt(encryptedPassword, masterPassword);
+      response.send(password);
+    } catch (error) {
+      console.error(error);
+      response.status(500).send(error.message);
+    }
   });
 
   app.post("/api/passwords", async (request, response) => {
